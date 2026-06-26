092f1a6a73f1f9db2e4340a7fd21b1f50994c368
Bizuayehu18 <bizuayehuasefa25@gmail.com>
2026-06-26 17:22:17 +0300

    Remove placeholder login security route

--- src/routes/_app/profile/security/login.tsx
+++ src/routes/_app/profile/security/login.tsx
@@ -1,9 +1,1 @@
-import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_app/profile/security/login")({
-  component: LoginSecurityPage,
-});
-
-function LoginSecurityPage() {
-  return <div>Login Security</div>;
-}
